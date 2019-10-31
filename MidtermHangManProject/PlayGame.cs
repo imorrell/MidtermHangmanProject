cff979b254adc0ede579147c073b11069f67405a
Ivoire Morrell <imorrell@ltu.edu>
2019-10-31 14:06:52 -0400

    Program updates

--- MidtermHangManProject/PlayGame.cs
+++ MidtermHangManProject/PlayGame.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace MidtermHangManProject
@@ -11,18 +12,45 @@ namespace MidtermHangManProject
         {
             //create and intialize variables
             bool ok = true;
-
-            //UserPlayer user = new UserPlayer();
+            string userName;
+            //char [] word = new char[];
+            List<String> userEntries = new List<string>();
+            int numberOfGuesses = 7;
+            string enterLetter;
+            int wins = 0;
+            int loses = 0;
+            
+            UserPlayer user = new UserPlayer();
 
             //welcome the user to the program
             Console.WriteLine("Welcome to Hangman! Get ready to take this L!\n");
 
             //Ask the user to enter their name
-            Console.WriteLine("Please enter your name\n");
+            userName = Validator.GetUserInputTwo("Please enter user name\n");
+
+            //add the username to the user object
+            user.Name = userName;
+
+            //use while loop to process user input
+            while (ok && numberOfGuesses != 0)
+            {
+                //set the name of the word from the file
+
+                
 
 
+
+
+
+
+
+            }
+
+            
 
         }
+
+        //The DisplayUnderScore met method
 
     }
 }
