c572b23a96580e177b8a4438afe0222047fcc2f9
Ivoire Morrell <imorrell@ltu.edu>
2019-10-31 11:06:49 -0400

    Setting up repo

---
+++ MidtermHangManProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MidtermHangManProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
