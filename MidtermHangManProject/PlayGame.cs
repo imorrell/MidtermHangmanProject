2e8d23d88dc6b84ce09930165189c4fd7925d4a0
Ivoire Morrell <imorrell@ltu.edu>
2019-10-31 12:05:04 -0400

    Ivo changes

---
+++ MidtermHangManProject/PlayGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidtermHangManProject
+{
+    class PlayGame
+    {
+        //The PlayHangMan method controls all the gameplay for hangman
+        public static void PlayHangMan()
+        {
+            //create and intialize variables
+            bool ok = true;
+
+            //UserPlayer user = new UserPlayer();
+
+            //welcome the user to the program
+            Console.WriteLine("Welcome to Hangman! Get ready to take this L!\n");
+
+            //Ask the user to enter their name
+            Console.WriteLine("Please enter your name\n");
+
+
+
+        }
+
+    }
+}
