c76740f5b23c0d3473b27e734f3ab44d80b327ee
tcotter1432 <tcotter1432@gmail.com>
2019-10-31 11:59:41 -0400

    Pushing first IO class and text files

---
+++ MidtermHangManProject/FileIO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace MidtermHangManProject
+{
+    class FileIO
+    {
+        private static string WordFile = "../../../Words.txt";
+        private static string UsersFile = "../../../UserPlayers.txt";
+        public List<string> Words { get; set; }
+        public List<UserPlayer> Users { get; set; }
+        //public List<string> EasyWords {get;set;}
+        //public List<string> HardWords {get;set;}
+
+        public static void WriteUsers()
+        {
+
+        }
+        public static void ReadUsers()
+        {
+
+        }
+        public static void ReadWords()
+        {
+
+        }
+        public static void WriteWords()
+        {
+
+        }
+    }
+}
